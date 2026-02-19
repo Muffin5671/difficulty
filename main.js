2911b1b78036355b4c7f050140bb81cda32ee042
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-02-19 11:26:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function searchLevels() {
   let query = encodeURI(document.getElementById('searchBar').value);
   window.location.href = `search/?query=${query}`;
 }
+
+function searchUsers() {
+  let query = encodeURI(document.getElementById('searchBar').value);
+  window.location.href = `search/?query=${query}&user=true`;
+}
