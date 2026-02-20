91c4c48b744ff5359210d47cd142ce5102b92758
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-02-20 12:38:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,9 @@ function searchLevels() {
 
 function searchUsers() {
   let query = encodeURI(document.getElementById('searchBar').value);
-  window.location.href = `search/?query=${query}&user=1`;
+  if (!(query == '')) {
+    window.location.href = `search/?query=${query}&user=1`;
+  } else {
+    console.warn('You cannot search for users without a search value.');
+  }
 }
