04ebb7793355a51a0cce6bf0ce182ec827eab2a2
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-02-24 17:07:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var ratedFilterEnabled = false;
 function searchLevels() {
   let query = encodeURI(document.getElementById('searchBar').value);
   if (ratedFilterEnabled) {
-    window.location.href = `search/?query=${query}&star=${ratedFilterEnabled}`;
+    window.location.href = `search/?query=${query}&star=${Number(ratedFilterEnabled)}`;
   } else {
     window.location.href = `search/?query=${query}`;
   }
@@ -19,7 +19,11 @@ function searchUsers() {
 function toggleRated() {
   if (ratedFilterEnabled == false) {
     ratedFilterEnabled = true;
+    document.getElementById('ratedLVLSButton').style.filter = '';
+    document.getElementById('ratedLVLSButton').title = 'Rated Levels [ON]'
   } else {
     ratedFilterEnabled = false;
+    document.getElementById('ratedLVLSButton').style.filter = 'grayscale(1)';
+    document.getElementById('ratedLVLSButton').title = 'Rated Levels [OFF]'
   }
 }
